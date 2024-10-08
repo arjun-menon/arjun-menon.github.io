d7909de2971d009d0d68a70a04586eb3fcc8737a
Arjun G. Menon <contact@arjungmenon.com>
2024-10-08 03:03:00 -0400

    build.fish: Ignore .obsidian

--- build.fish
+++ build.fish
@@ -1,2 +1,2 @@
 #!/usr/bin/env fish
-alteza --content . --output test_site --clear_output_dir --watch --ignore test_site .venv .git
+alteza --content . --output test_site --clear_output_dir --watch --ignore test_site .venv .git .obsidian
