ce8c15cf2272ac96fca8566af18f5d6a1867449d
Arjun G. Menon <contact@arjungmenon.com>
2025-04-23 21:16:43 -0400

    Adjust desktop bg color

--- __config__.py
+++ __config__.py
@@ -6,8 +6,9 @@ max_content_width = '45em'
 
 yellowish_1 = '254, 247, 239' #FEF7EF
 whiteish_1 = '254, 254, 254' #FEFEFE
+orangeish_1 = '255, 234, 207' #ffeacf
 
 content_bgcolor = yellowish_1
-body_bgcolor = yellowish_1
+body_bgcolor = orangeish_1
 bgcolor = content_bgcolor
 
