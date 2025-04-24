784aae8f902f853ebb5aca167291bed394020321
Arjun G. Menon <contact@arjungmenon.com>
2025-04-23 21:09:43 -0400

    Add textured-paper background in desktop

--- __config__.py
+++ __config__.py
@@ -4,7 +4,10 @@ lr_padding = '1.7em'
 
 max_content_width = '45em'
 
-yellowish_1 = '254, 247, 239'
-whiteish_1 = '254, 254, 254'
-bgcolor = yellowish_1
+yellowish_1 = '254, 247, 239' #FEF7EF
+whiteish_1 = '254, 254, 254' #FEFEFE
+
+content_bgcolor = yellowish_1
+body_bgcolor = yellowish_1
+bgcolor = content_bgcolor
 
